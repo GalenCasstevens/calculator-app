5125c6476a5dfb151db1df24d8cf01c65f08f515
Galen Casstevens <galencasstevens@gmail.com>
2023-02-20 13:52:15 -0500

    added functionality to most of the buttons

--- src/App.js
+++ src/App.js
@@ -3,9 +3,54 @@ import './App.css';
 
 const App = () => {
 	const [result, setResult] = useState('');
+	const [operator, setOperator] = useState('');
 
-	const handleClick = (e) => {
+	const clearResult = () => {
+		setResult('');
+	};
+
+	const buildExpression = (e) => {
 		setResult(result.concat(e.target.name));
+	};
+
+	const changeOperator = (e) => {
+		// does not make sense to have operand as first input
+		if (result.length === 0) return;
+
+		const newOperator = e.target.name;
+
+		if (isNewOperand()) {
+			updateOperator(newOperator);
+			setOperator(newOperator);
+		} else {
+			setOperator(newOperator);
+			setResult(result.concat(newOperator));
+		}
+	};
+
+	const updateOperator = (newOperator) => {
+		const ind = result.lastIndexOf(operator);
+		const newResult = result.substring(0, ind) + newOperator;
+		setResult(newResult);
+	};
+
+	const calcResult = (e) => {
+		if (isNewOperand()) setResult(eval(result.substring(0, result.length - 1)));
+		else setResult(eval(result).toString());
+	};
+
+	const isNewOperand = () => {
+		const lastChar = result.slice(-1);
+
+		if (
+			lastChar === '/' ||
+			lastChar === '*' ||
+			lastChar === '+' ||
+			lastChar === '-'
+		)
+			return true;
+
+		return false;
 	};
 
 	return (
@@ -16,65 +61,65 @@ const App = () => {
 				</form>
 
 				<div className="keypad">
-					<button className="red-btn" onClick={handleClick}>
+					<button className="red-btn" onClick={clearResult}>
 						C
 					</button>
-					<button className="red-btn" onClick={handleClick}>
+					<button className="red-btn" onClick={buildExpression}>
 						+/-
 					</button>
-					<button className="red-btn" onClick={handleClick}>
+					<button className="red-btn" onClick={buildExpression}>
 						%
 					</button>
-					<button className="yellow-btn" name="/" onClick={handleClick}>
+					<button className="yellow-btn" name="/" onClick={changeOperator}>
 						&divide;
 					</button>
-					<button className="blue-btn" name="7" onClick={handleClick}>
+					<button className="blue-btn" name="7" onClick={buildExpression}>
 						7
 					</button>
-					<button className="blue-btn" name="8" onClick={handleClick}>
+					<button className="blue-btn" name="8" onClick={buildExpression}>
 						8
 					</button>
-					<button className="blue-btn" name="9" onClick={handleClick}>
+					<button className="blue-btn" name="9" onClick={buildExpression}>
 						9
 					</button>
-					<button className="yellow-btn" name="*" onClick={handleClick}>
+					<button className="yellow-btn" name="*" onClick={changeOperator}>
 						&times;
 					</button>
-					<button className="blue-btn" name="4" onClick={handleClick}>
+					<button className="blue-btn" name="4" onClick={buildExpression}>
 						4
 					</button>
-					<button className="blue-btn" name="5" onClick={handleClick}>
+					<button className="blue-btn" name="5" onClick={buildExpression}>
 						5
 					</button>
-					<button className="blue-btn" name="6" onClick={handleClick}>
+					<button className="blue-btn" name="6" onClick={buildExpression}>
 						6
 					</button>
-					<button className="yellow-btn" name="-" onClick={handleClick}>
-						&ndash;
+					<button className="yellow-btn" name="+" onClick={changeOperator}>
+						+
 					</button>
-					<button className="blue-btn" name="1" onClick={handleClick}>
+					<button className="blue-btn" name="1" onClick={buildExpression}>
 						1
 					</button>
-					<button className="blue-btn" name="2" onClick={handleClick}>
+					<button className="blue-btn" name="2" onClick={buildExpression}>
 						2
 					</button>
-					<button className="blue-btn" name="3" onClick={handleClick}>
+					<button className="blue-btn" name="3" onClick={buildExpression}>
 						3
 					</button>
-					<button className="yellow-btn" name="+" onClick={handleClick}>
-						+
+					<button className="yellow-btn" name="-" onClick={changeOperator}>
+						&ndash;
 					</button>
 					<button
 						className="grid-span-2 blue-btn"
 						name="0"
-						onClick={handleClick}
+						onClick={buildExpression}
 					>
 						0
 					</button>
-					<button className="blue-btn" name="." onClick={handleClick}>
+					<button className="blue-btn" name="." onClick={buildExpression}>
 						.
 					</button>
-					<button className="yellow-btn" name="=" onClick={handleClick}>
+					<button className="yellow-btn" name="=" onClick={calcResult}>
 						=
 					</button>
 				</div>
